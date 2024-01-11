468bbbda0d7a39b988153c58f33c57b74bf7086b
AlbertoDeTeresaChaves <alberto.trabajo.es@gmail.com>
2024-01-11 13:48:36 +0100

    Prueba inicio aplicacion

---
+++ Ejer01.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk
+
+class Application(ttk.Frame):
+    def __init__(self,main_window):
+        super().__init__(main_window)
+        main_window.title("Posicionar elementos en Tcl/Tk")
+        main_window.configure(width=300,height=200)
+        self.place(relwidth=1,relheight=1)
+#MAIN     
+principal = tk.Tk()
+app = Application(principal)
+app.mainloop()
