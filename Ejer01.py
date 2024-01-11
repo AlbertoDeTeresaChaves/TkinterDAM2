ecc295924c79c490c5c19ae8515bcc27783ba3a6
AlbertoDeTeresaChaves <alberto.trabajo.es@gmail.com>
2024-01-11 13:53:46 +0100

    Agregacion de un boton

--- Ejer01.py
+++ Ejer01.py
@@ -6,6 +6,8 @@ class Application(ttk.Frame):
         super().__init__(main_window)
         main_window.title("Posicionar elementos en Tcl/Tk")
         main_window.configure(width=300,height=200)
+        self.button = ttk.Button(self,text="Hola, mundo!")
+        self.button.place(x=60, y=40, width=100,height=30)
         self.place(relwidth=1,relheight=1)
 #MAIN     
 principal = tk.Tk()
