c23b65b1c3772fc68567634582fde87598069d98
AlbertoDeTeresaChaves <alberto.trabajo.es@gmail.com>
2024-01-22 10:23:44 +0100

    Estado final de las temperaturas

--- Ejercicio Temperaturas/CalculoTemperatura.py
+++
@@ -1,2 +0,0 @@
-class Calculos:
-    print("Hola")
--- Ejercicio Temperaturas/Temperaturas.py
+++ Ejercicio Temperaturas/Temperaturas.py
@@ -2,92 +2,90 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox as mb
 from tkinter import PhotoImage
-import CalculoTemperatura
+import convertir as cv
 
 class Temperaturas:
 
-    def __init__(self):
-        self.ctemperatura=CalculoTemperatura.Calculos
-        self.ventana=tk.Tk()
-        self.ventana.title("Calcular Temperatura")
-        self.ventana.resizable(False,False)
-        self.ventana.configure(background="#70A3CC")
-        w = 525
-        h = 400
-        x = (self.ventana.winfo_screenwidth() /2) - (w /2)
-        y = (self.ventana.winfo_screenheight() /2) - (w /2)
-        self.ventana.geometry('%dx%d+%d+%d' % (w,h,x,y))
-        self.Ingresar()
-        self.ventana.mainloop()
+    def __init__(self,ventana):
 
+        s = ttk.Style()
+        s.theme_use("default")
+        s.configure("TLabelframe",background="grey")
+        s.configure("TLabelframe.Label", background="grey",foreground="white",font=("verdana",20,"bold"))
+        s.configure("TButton",background="grey",foreground="white",font=("verdana",15,"bold"))
 
-    def Ingresar(self):
-        #solecito = PhotoImage(file="Ejercicios Temperaturas/sol.png")
-        #self.sol_label=ttk.Label(image=solecito)
-        #self.sol_label.grid(column=0,row=0)
+        caracteristicas = {"justify":"left", "background":"#FFFFFF","foreground":"black","font":("Verdana",10,"bold"),"width":20}
+        caracteristicas2 = {"justify":"right", "background":"#FFFFFF","foreground":"black","font":("Verdana",10,"bold"),"width":10}
+        caracteristicas3 = {"width":3}
+
+        self.lf1 = ttk.LabelFrame(ventana,text="Conversiones",labelanchor="nw")
+        self.lf1.grid(column=0,row=0,padx=5)
         #==============================CELSIUS======================================
-        self.celsius_label =ttk.Label(self.ventana,text="Celsius",background="#70A3CC",font="20")
-        self.celsius_label.grid(column=0,row=0,padx=4,pady=4)
+        ttk.Label(self.lf1,text="Celsius",**caracteristicas).grid(column=0,row=0,padx=20,pady=20)
 
-        self.celsius_valor=tk.DoubleVar()
-        self.celsius_entry= ttk.Entry(self.ventana,textvariable=self.celsius_valor,width=15)
-        self.celsius_entry.grid(column=1,row=0,padx=4,pady=4)
+        self.gc=tk.DoubleVar()
+        caja1 = ttk.Entry(self.lf1,textvariable=self.gc,**caracteristicas2)
+        caja1.grid(column=1,row=0)
+        caja1.bind("<Return>",lambda*_,:self.cambio(*_,v1="gc",v2=0))
+        caja1.bind("<FocusOut>",lambda*_,:self.cambio(*_,v1="gc",v2=0))
         
-
-        self.botonmas=ttk.Button(self.ventana,text="+1",width=4,command=self.sumar_c)
-        self.botonmas.grid(column=2,row=0,padx=4,pady=4)
-        self.botonmenos=ttk.Button(self.ventana,text="-1",width=4,command=self.restar_c)
-        self.botonmenos.grid(column=3,row=0,padx=4,pady=4)
+        ttk.Button(self.lf1,text="+1",command=lambda *_: self.cambio(v1="gc",v2=1),**caracteristicas3).grid(column=2,row=0,padx=10)
+        ttk.Button(self.lf1,text="-1",command=lambda *_: self.cambio(*_,v1="gc",v2=-1),**caracteristicas3).grid(column=3,row=0,padx=4,pady=4)
 
         #==============================FARENHEIT====================================
-        self.farenheit_label =ttk.Label(self.ventana,text="Fahrenheit",background="#70A3CC",font="20")
-        self.farenheit_label.grid(column=0,row=1,padx=4)
-        self.farenheit_valor=tk.DoubleVar()
-        self.farenheit_entry= ttk.Entry(self.ventana,textvariable=self.farenheit_valor,width=15)
-        self.farenheit_entry.grid(column=1,row=1,padx=4,pady=4)
+        ttk.Label(self.lf1,text="Fahrenheit",**caracteristicas).grid(column=0,pady=20)
 
+        self.gf=tk.DoubleVar()
+        caja2= ttk.Entry(self.lf1,textvariable=self.gf,**caracteristicas2)
+        caja2.grid(column=1,row=1)
+        caja2.bind("<Return>",lambda*_,:self.cambio(*_,v1="gf",v2=0))
+        caja2.bind("<FocusOut>",lambda*_,:self.cambio(*_,v1="gf",v2=0))
 
-        self.botonmas=ttk.Button(self.ventana,text="+1",width=4,command=self.sumar_f)
-        self.botonmas.grid(column=2,row=1,padx=4,pady=4)
-        self.botonmenos=ttk.Button(self.ventana,text="-1",width=4,command=self.restar_f)
-        self.botonmenos.grid(column=3,row=1,padx=4,pady=4)
+        ttk.Button(self.lf1,text="+1",command=lambda *_: self.cambio(*_,v1="gf",v2=1),**caracteristicas3).grid(column=2,row=1)
+        ttk.Button(self.lf1,text="-1",command=lambda *_: self.cambio(*_,v1="gf",v2=-1),**caracteristicas3).grid(column=3,row=1)
         
         #==============================KELVIN======================================
-        self.kelvin_label =ttk.Label(self.ventana,text="Kelvin",background="#70A3CC",font="20")
-        self.kelvin_label.grid(column=0,row=2,padx=4)
-        self.kelvin_valor=tk.DoubleVar()
-        self.kelvin_entry= ttk.Entry(self.ventana,textvariable=self.kelvin_valor,width=15)
-        self.kelvin_entry.grid(column=1,row=2,padx=4,pady=4)
+        ttk.Label(self.lf1,text="Kelvin",**caracteristicas).grid(column=0,pady=20)
+
+        self.gk=tk.DoubleVar()
+        caja3= ttk.Entry(self.lf1,textvariable=self.gk,**caracteristicas2)
+        caja3.grid(column=1,row=2)
+        caja3.bind("<Return>",lambda*_,:self.cambio(*_,v1="gk",v2=0))
+        caja3.bind("<FocusOut>",lambda*_,:self.cambio(*_,v1="gk",v2=0))
         
+        ttk.Button(self.lf1,text="+1",command=lambda *_: self.cambio(*_,v1="gk",v2=1),**caracteristicas3).grid(column=2,row=2)
+        ttk.Button(self.lf1,text="-1",command=lambda *_: self.cambio(*_,v1="gk",v2=-1),**caracteristicas3).grid(column=3,row=2)
 
-        self.botonmas=ttk.Button(self.ventana,text="+1",width=4,command=self.sumar_k)
-        self.botonmas.grid(column=2,row=2,padx=4,pady=4)
-        self.botonmenos=ttk.Button(self.ventana,text="-1",width=4,command=self.restar_k)
-        self.botonmenos.grid(column=3,row=2,padx=4,pady=4)
+    def cambio(self,*args,v1,v2):
+        match v1:
+            case "gc":
+                gc=self.gc.get()+v2
+                gf=cv.Convertir.c_a_f(gc,False)
+                gk=cv.Convertir.c_a_k(gc,False)
+            case "gf":
+                gf=self.gc.get()+v2
+                gc=cv.Convertir.f_a_c(gf,False)
+                gk=cv.Convertir.f_a_k(gf,False)
+            case "gk":
+                gk=self.gc.get()+v2
+                gf=cv.Convertir.k_a_f(gk,False)
+                gck=cv.Convertir.k_a_c(gk,False)
+        self.gc.set(gc)
+        self.gf.set(gf)
+        self.gk.set(gk)
 
-        #celsius_entry.trace("w",funcion)
-        self.celsius_valor.trace_add("write",self.actualizar_C())
-        self.farenheit_valor.trace_add("write",self.actualizar_F())
-        self.kelvin_valor.trace_add("write",self.actualizar_K())
-#==================================SUMAR-RESTAR===================================
-    def sumar_c(self): self.celsius_valor.set(self.celsius_valor.get()+1)
-    def sumar_f(self): self.farenheit_valor.set(self.farenheit_valor.get()+1)
-    def sumar_k(self): self.kelvin_valor.set(self.kelvin_valor.get()+1)
-    def restar_c(self): self.celsius_valor.set(self.celsius_valor.get()-1)
-    def restar_f(self): self.farenheit_valor.set(self.farenheit_valor.get()-1)
-    def restar_k(self): self.kelvin_valor.set(self.kelvin_valor.get()-1)
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.configure(padx=30,pady=30)
+        self.title("Conversor Temperaturas")
+        w = 525
+        h = 400
+        x = (self.winfo_screenwidth() /2) - (w /2)
+        y = (self.winfo_screenheight() /2) - (w /2)
+        self.geometry('%dx%d+%d+%d' % (w,h,x,y))
+        self.resizable(False,False)
 
-    def actualizar_C(self):
-        #mb.showinfo("Alerta",self.celsius_entry.get())
-        self.farenheit_valor.set(((self.celsius_valor.get()*9)/5)+32)
-        self.kelvin_valor.set(self.celsius_valor.get() + 273.15)
-
-    def actualizar_F(self):
-        self.celsius_valor.set(((self.farenheit_valor.get()-32)*5) /9)
-        self.kelvin_valor.set(((self.farenheit_valor.get()-32)*5)/9 + 273.15)
-
-    def actualizar_K(self):
-        self.celsius_valor.set(self.kelvin_valor.get() -273.15)
-        self.farenheit_valor.set(((self.kelvin_valor.get()-273.15)*9)/5 + 32)
-
-app=Temperaturas()
+app=App()
+Temperaturas(app)
+app.mainloop()
