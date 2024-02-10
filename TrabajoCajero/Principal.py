3a4a94edb7a9cb6708e6cd65640f5474f86c3876
alberto <alberto.trabajo.es@gmail.com>
2024-02-10 23:50:21 +0100

    Principios de la primera parte

--- TrabajoCajero/Principal.py
+++ TrabajoCajero/Principal.py
@@ -1,24 +1,20 @@
 from tkinter import *
 
-
-
 class Inicio(Frame):
-    def __init__(self,ventana):
-        self.titulo=Label(ventana,text="CAJERO",background="#5A5352",font=30,justify="center").grid(column=0,columnspan=10)
+    def __init__(self, ventana):
+        super().__init__(ventana)
+        self.config(background="#5A5352")
+        self.grid(column=0, row=0, padx=200, pady=10, sticky=(N, W, E, S))        
+        textobold = {"foreground": "white", "background": "#5A5352", "font": "helvetica 11 bold"}
+        
+        Label(self, text="MONEDA", **textobold).grid(column=0, row=0, padx=10, pady=5)
+        Label(self, text="CANTIDAD", **textobold).grid(column=1, row=0, padx=10, pady=5)
 
-        self.verCajero=Label(ventana,text="Ver Cajero",foreground="white",background="#5A5352").grid(column=0,row=1)
-        self.bMostrarCajero=Button(ventana,text="VER",background="RED",command=self.cambio(Inicio2))
-        self.bMostrarCajero.grid(column=0,row=2,padx=15)
+        monedas = ["500.00", "200.00", "100.00", "50.00", "20.00", "10.00", "5.00", "2.00", "1.00","0.50", "0.20", "0.10", "0.05", "0.02", "0.01"]
 
-
-    def cambio(self,opcion):
-        new_frame=opcion(self)
-        self._frame = new_frame
-        self._frame.pack()
-
-class Inicio2(Frame):
-    def __init__(self,ventana):
-        self.titulo=Label(ventana,text="HOLA").grid(column=0,row=0)
+        for i, moneda in enumerate(monedas, start=1):
+            Label(self, text=moneda, **textobold).grid(column=0, row=i)
+            Spinbox(self, from_=0, to=100, width=10).grid(column=1, row=i)
 
 
 class App(Tk):
@@ -29,12 +25,11 @@ class App(Tk):
         h = 400
         x = (self.winfo_screenwidth() / 2) - (w / 2)
         y = (self.winfo_screenheight() / 2) - (h / 2)
-        self.geometry('%dx%d+%d+%d' % (w,h,x,y))
-        self.resizable(False,False)
+        self.geometry('%dx%d+%d+%d' % (w, h, x, y))
+        self.resizable(False, False)
         self.configure(background="#5A5352")
 
 
-
-app=App()
+app = App()
 Inicio(app)
 app.mainloop()
